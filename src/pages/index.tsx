7dbe83aadbcece64adda3c0829d5928299149b79
jay <devjay0530@gmail.com>
2023-03-24 23:41:15 +0900

    change index main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,11 @@ import { motion } from 'framer-motion';
 import React from 'react';
 
 import LinkArrow from '~/components/common/LinkArrow';
-import PlainText from '~/components/common/PlainText';
 import Title from '~/components/common/Title';
 import BouncingBall from '~/components/game/BouncingBall';
 import Layout from '~/components/layouts/Layout';
 import { PageSEO } from '~/components/SEO';
-import { fadeIn, fadeInHalf, staggerHalf } from '~/constants/animations';
+import { fadeIn, staggerHalf } from '~/constants/animations';
 
 export default function Home() {
   return (
@@ -15,18 +14,8 @@ export default function Home() {
       <PageSEO />
 
       <motion.section variants={staggerHalf} initial="initial" animate="animate">
-        <Title>lavinoys</Title>
-
-        <motion.div variants={fadeInHalf} className="mb-4">
-          Keep Going Test
-        </motion.div>
-
-        <motion.div variants={fadeInHalf}>
-          <PlainText>Test.</PlainText>
-        </motion.div>
+        <Title>Blog</Title>
       </motion.section>
-
-      <BouncingBall />
 
       <motion.section className="mt-16" variants={staggerHalf} initial="initial" animate="animate">
         <motion.div variants={fadeIn}>
@@ -35,6 +24,8 @@ export default function Home() {
           </LinkArrow>
         </motion.div>
       </motion.section>
+
+      <BouncingBall />
     </Layout>
   );
 }
